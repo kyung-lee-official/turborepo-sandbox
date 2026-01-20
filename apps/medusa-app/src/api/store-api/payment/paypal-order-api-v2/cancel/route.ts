06fd199fa90bd0663bc5e518ba005f7c09b8e048
Kyung Lee <kyung.lee.official@outlook.com>
2026-01-20 17:09:50 +0800

    chore: ...

--- apps/medusa-app/src/api/store-api/payment/paypal-order-api-v2/cancel/route.ts
+++
@@ -1,5 +0,0 @@
-import type { MedusaRequest, MedusaResponse } from "@medusajs/framework";
-
-export async function GET(req: MedusaRequest, res: MedusaResponse) {
-  res.send("PayPal Order API V2 Return Route");
-}
