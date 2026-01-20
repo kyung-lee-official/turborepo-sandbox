c89d1591534bc0da55bc1d3b883df35610590089
Kyung Lee <kyung.lee.official@outlook.com>
2026-01-21 07:57:22 +0800

    wip: enhance PayPal webhook handling with event type processing and improved error messaging

--- apps/medusa-app/src/api/store-api/payment/paypal-order-api-v2/webhooks/route.ts
+++ apps/medusa-app/src/api/store-api/payment/paypal-order-api-v2/webhooks/route.ts
@@ -1,8 +1,13 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework";
-import { HttpError } from "@repo/types";
+import { HttpError, type PayPalWebhookEvent } from "@repo/types";
 import { verifyWebhookSignature } from "./verify-webhook-signature";
 
+/**
+ * If PayPal webhook failed to hit this endpoint, PayPal marks the webhook event as FAIL_SOFT, 
+ * and will retry sending the webhook event for a certain period of time (typically around 1 minute).
+ */
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
+  const body = req.body as PayPalWebhookEvent;
   try {
     const isValid = await verifyWebhookSignature(
       req.headers as Record<string, string>,
@@ -11,9 +16,25 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
     if (!isValid) {
       throw new HttpError("PAYMENT.PAYPAL_INVALID_WEBHOOK_SIGNATURE");
     }
-    res.send({ status: "Webhook received and verified" });
+    switch (body.event_type) {
+      case "CHECKOUT.ORDER.APPROVED":
+        break;
+      case "PAYMENT.AUTHORIZATION.CREATED":
+        break;
+      case "PAYMENT.CAPTURE.COMPLETED":
+        break;
+      default:
+        /* ignore unknown event types */
+        break;
+    }
+    console.log(body.event_type);
+    res.status(200).send({ status: "Webhook received and verified" });
+    return;
   } catch (error) {
-    throw new HttpError("PAYMENT.PAYPAL_INVALID_WEBHOOK_SIGNATURE");
+    throw new HttpError(
+      "PAYMENT.PAYPAL_INVALID_WEBHOOK_SIGNATURE",
+      "Caution, invalid PayPal webhook signature",
+      error,
+    );
   }
-  return;
 }
