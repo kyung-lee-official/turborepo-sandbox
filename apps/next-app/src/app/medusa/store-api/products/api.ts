4a30c65ad65eba532d7c27d1f712584ba365fba6
Kyung Lee <kyung.lee.official@outlook.com>
2026-02-02 08:34:06 +0800

    fix(next-app): update scripts to use Bun and add NODE_USE_SYSTEM_CA to fix NodeJS self-signed CA issue

--- apps/next-app/src/app/medusa/store-api/products/api.ts
+++ apps/next-app/src/app/medusa/store-api/products/api.ts
@@ -8,6 +8,12 @@ export async function getProduts() {
   const data = await api.get<StoreProductListResponse>(`/store/products`);
   return data;
 }
+// export async function getProduts(): Promise<StoreProductListResponse> {
+//   const response = await fetch("https://medusa.sandbox.local/store/products");
+//   const data = (await response.json()) as StoreProductListResponse;
+//   return data;
+// }
+
 
 export async function getProductById(productId: string) {
   const data = await api.get<StoreProductResponse>(
