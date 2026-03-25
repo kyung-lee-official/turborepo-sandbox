c43035b8d7878417b39cd3a2237badf9c40144c1
Kyung Lee <kyung.lee.official@outlook.com>
2026-03-25 08:25:06 +0800

    fix: block native POST /store/carts

--- apps/medusa-app/src/api/store-api/carts/middlewares.ts
+++ apps/medusa-app/src/api/store-api/carts/middlewares.ts
@@ -1,8 +1,12 @@
 import {
+  type MedusaRequest,
+  type MedusaResponse,
   type MiddlewareRoute,
   validateAndTransformBody,
   validateAndTransformQuery,
 } from "@medusajs/framework/http";
+import { HttpError } from "@repo/types";
+import type { NextFunction } from "express";
 import * as QueryConfig from "./query-config";
 import { StoreCreateCart, StoreGetCartsCart } from "./validators";
 
@@ -16,6 +20,19 @@ export const storeCartRoutesMiddlewares: MiddlewareRoute[] = [
         QueryConfig.retrieveTransformQueryConfig,
       ),
       validateAndTransformBody(StoreCreateCart),
+    ],
+  },
+  // block the old route for carts to avoid confusion
+  {
+    methods: ["POST"],
+    matcher: "/store/carts",
+    middlewares: [
+      (req: MedusaRequest, res: MedusaResponse, next: NextFunction) => {
+        throw new HttpError(
+          "AUTH.FORBIDDEN",
+          "This route has been disabled because the metadata was not initialized properly. Please use `POST /store-api/carts` instead.",
+        );
+      },
     ],
   },
 ];
