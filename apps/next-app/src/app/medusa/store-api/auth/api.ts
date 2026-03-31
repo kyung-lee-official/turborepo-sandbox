84f59e8eec65f7dd21b3ee2dc1f43d45c56eb42a
Kyung Lee <kyung.lee.official@outlook.com>
2026-03-31 09:46:17 +0800

    feat (next-app): implement session management endpoints and update authentication methods

--- apps/next-app/src/app/medusa/store-api/auth/api.ts
+++ apps/next-app/src/app/medusa/store-api/auth/api.ts
@@ -1,14 +1,50 @@
+import { AxiosHeaders } from "axios";
 import api from "../../axios-error-handling-for-medusa/axios-client";
 
+/**
+ * Medusa's built-in JWT authentication endpoint
+ */
 export async function authenticateCustomer(email: string, password: string) {
-  const data = await api.post(`/auth/sign-in/customer/emailpass`, {
+  const data = await api.post(`/auth/customer/emailpass`, {
     email,
     password,
   });
   return data;
 }
 
-export async function signOutCustomer() {
-  const data = await api.del(`/auth/sign-out`);
+/**
+ * Medusa's built-in session endpoint
+ */
+export async function getSession(jwt: string) {
+  const headers = new AxiosHeaders();
+  headers.set("Authorization", `Bearer ${jwt}`);
+  const data = await api.post(`/auth/session`, {}, { headers });
   return data;
 }
+
+/**
+ * Medusa's built-in session deletion endpoint
+ */
+export async function deleteSession(jwt: string) {
+  const data = await api.del(`/auth/session`);
+  return data;
+}
+
+/**
+ * Sign in with the HttpOnly Cookie way to store JWT
+ */
+// export async function authenticateCustomer(email: string, password: string) {
+//   const data = await api.post(`/auth/sign-in/customer/emailpass`, {
+//     email,
+//     password,
+//   });
+//   return data;
+// }
+
+/**
+ * Sign out by deleting the HttpOnly Cookie
+ */
+// export async function signOutCustomer() {
+//   const data = await api.del(`/auth/sign-out`);
+//   return data;
+// }
