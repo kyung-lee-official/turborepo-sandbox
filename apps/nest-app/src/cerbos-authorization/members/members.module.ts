190d16fc49b473bcf7b2937fea66d5a671e75215
Kyung Lee <kyung.lee.official@outlook.com>
2026-08-28 15:56:39 +0800

    refactor: remove Cerbos integration and related configurations from environment files and modules

--- apps/nest-app/src/cerbos-authorization/members/members.module.ts
+++ apps/nest-app/src/cerbos-authorization/members/members.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "@/recipes/prisma/prisma.module";
-import { FindAllCerbosGuard } from "./guards/find-all.guard";
 import { MembersController } from "./members.controller";
 import { MembersService } from "./members.service";
 
 @Module({
   imports: [PrismaModule],
   controllers: [MembersController],
-  providers: [MembersService, FindAllCerbosGuard],
+  providers: [MembersService],
 })
 export class MembersModule {}
--- apps/nest-app/src/cerbos-authorization/roles/roles.module.ts
+++ apps/nest-app/src/cerbos-authorization/roles/roles.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "@/recipes/prisma/prisma.module";
-import { FindAllCerbosGuard } from "./guards/find-all.guard";
 import { RolesController } from "./roles.controller";
 import { RolesService } from "./roles.service";
 
 @Module({
   imports: [PrismaModule],
   controllers: [RolesController],
-  providers: [RolesService, FindAllCerbosGuard],
+  providers: [RolesService],
 })
 export class RolesModule {}
